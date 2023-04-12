cb79efebafdc7d58d69084d56e31dff80a7310a1
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-13 00:17:58 +0600

    сделаны остальные методы для первого задания

--- task_1/urls.py
+++ task_1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from task_1.views import add_view
+from task_1.views import add_view, subtract_view, multiply_view, divide_view
 
 urlpatterns = [
-    path('add/', add_view, name='add')
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide'),
 ]
--- task_1/views.py
+++ task_1/views.py
@@ -16,3 +16,48 @@ def add_view(request, *args, **kwargs):
             response = JsonResponse(response_data)
             response.status_code = 400
         return response
+
+
+@require_http_methods(['GET'])
+def subtract_view(request, *args, **kwargs):
+    if request.body:
+        data = json.loads(request.body)
+        try:
+            response = JsonResponse({
+                'answer': int(data['A']) - int(data['B'])
+            })
+        except ValueError:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+@require_http_methods(['GET'])
+def multiply_view(request, *args, **kwargs):
+    if request.body:
+        data = json.loads(request.body)
+        try:
+            response = JsonResponse({
+                'answer': int(data['A']) * int(data['B'])
+            })
+        except ValueError:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+@require_http_methods(['GET'])
+def divide_view(request, *args, **kwargs):
+    if request.body:
+        data = json.loads(request.body)
+        try:
+            response = JsonResponse({
+                'answer': int(data['A']) / int(data['B'])
+            })
+        except ValueError:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
